d837afab04da21233ce6c1b2d7f0530631e54707
EnesBatuhanHalac <halacenesbatuhan@gmail.com>
2024-01-26 14:05:40 +0300

    Loading img entegre edildi.Resimler Belirli bir scroll mesafesinden sonra yeniden api'dan veri çekilmesi için ayar yapildi

--- JavascriptProjeleri/PicturesFromApi/app.js
+++ JavascriptProjeleri/PicturesFromApi/app.js
@@ -1,10 +1,11 @@
-const accessKey="Jm9QDGXF5iuHyySqCsccgP9zjegtXpomezQot154Sro";
+const accessKey="b1UX1o-OMtL1CERrgsut3MIFb2FCyOiqExSP32z-GdM";
 const countImage=30;
 const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=${countImage}`;
 let imagesList=[];
 const imageContanier=document.getElementById("imageContainer");
-
-
+const loader=document.querySelector('.loadingContainer');
+let count =0;
+let totalImages=0;
 async function getImages(){
   try {
     const response = await fetch(apiUrl);
@@ -19,6 +20,8 @@ async function getImages(){
 getImages();
 
 function addImages(){
+  totalImages=0;
+  totalImages = imagesList.length;
   imagesList.forEach(img=>{
     const a=document.createElement('a');
      a.setAttribute("href",img.urls.regular)
@@ -26,6 +29,21 @@ function addImages(){
      picture.setAttribute("src",img.urls.regular)
      picture.setAttribute("alt",img.alt_description)
      a.appendChild(picture);
+    picture.addEventListener("load",()=>{
+      count++;
+      if(count == totalImages){
+        loader.hidden=true;
+      }
+    })
      imageContanier.appendChild(a);
   })
 }
+
+
+
+window.addEventListener("scroll",()=>{
+  if(window.innerHeight + scrollY >=document.body.offsetHeight - 1000){
+    getImages();
+  }
+})
+
