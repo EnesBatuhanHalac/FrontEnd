089e57692d281eded84e4bac0ee72f0f5bda4991
EnesBatuhanHalac <halacenesbatuhan@gmail.com>
2024-01-26 12:13:35 +0300

    Api'dan resimler çekildi

--- JavascriptProjeleri/PicturesFromApi/app.js
+++ JavascriptProjeleri/PicturesFromApi/app.js
@@ -1,2 +1,31 @@
-const apiUrl="https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY";
-const accessKey="Jm9QDGXF5iuHyySqCsccgP9zjegtXpomezQot154Sro"
+const accessKey="Jm9QDGXF5iuHyySqCsccgP9zjegtXpomezQot154Sro";
+const countImage=30;
+const apiUrl=`https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=${countImage}`;
+let imagesList=[];
+const imageContanier=document.getElementById("imageContainer");
+
+
+async function getImages(){
+  try {
+    const response = await fetch(apiUrl);
+    imagesList= await response.json();
+    // console.log(imagesList)
+      addImages();
+  } catch (error) {
+    
+  }
+}
+
+getImages();
+
+function addImages(){
+  imagesList.forEach(img=>{
+    const a=document.createElement('a');
+     a.setAttribute("href",img.urls.regular)
+     const picture=document.createElement('img');
+     picture.setAttribute("src",img.urls.regular)
+     picture.setAttribute("alt",img.alt_description)
+     a.appendChild(picture);
+     imageContanier.appendChild(a);
+  })
+}
